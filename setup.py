c098bd136d5634d08ea8c5cea6a16ea57c4ce10c
Adrian Usler <adrian.usler@googlemail.com>
2024-08-08 23:04:10 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from massaction import __version__
+import setuptools
+
+with open("README.md", "r", encoding='utf8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="massaction",
+    version=__version__,    
+    description="A python package for the numerical solution of mass-action laws with constraints for the description of chemical reactions",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/adrianusler/massaction",
+    author="Adrian L. Usler",
+    author_email="adrian.usler@rwth-aachen.de",
+    license="MIT License",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "numpy",
+        "scipy",
+    ],
+    python_requires="~=3.8",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
