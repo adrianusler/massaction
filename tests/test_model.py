446c7906d685b149ddc0e601a125648749f4da79
Adrian Usler <adrian.usler@googlemail.com>
2024-08-11 20:34:07 +0200

    Implement test of get_num_sweep

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,7 +1,9 @@
-from massaction.model import ChemModel
+from massaction.model import ChemModel, get_num_sweep
 from massaction.species import Species
 
 from unittest import TestCase
+
+import pytest
 
 
 class TestChemModel(TestCase):
@@ -26,3 +28,13 @@ class TestChemModel(TestCase):
         assert isinstance(list_species, list)
         assert len(list_species) == 3
         assert all(isinstance(s, Species) for s in list_species)
+
+    def test_get_num_sweep(self):
+        h2o, h2, o2 = self.model3.get_all_species()
+        c1 = h2o == 1.0
+        c2 = h2 == 4.0
+        c3 = o2 == [1.0, 2.0, 3.0, 4.0, 5.0]
+        assert get_num_sweep([c1, c2, c3]) == 5
+        c2 = h2 == [10.0, 11.0, 12.0]
+        with pytest.raises(ValueError):
+            get_num_sweep([c1, c2, c3])
