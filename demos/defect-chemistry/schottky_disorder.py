1b09b4c194b40a7da0cc70d980fc151e427478b9
Adrian Usler <adrian.usler@googlemail.com>
2024-08-18 18:16:37 +0200

    Use nil syntax element in schottky_disorder.py demo

--- demos/defect-chemistry/schottky_disorder.py
+++ demos/defect-chemistry/schottky_disorder.py
@@ -1,22 +1,20 @@
-from massaction.model import ChemModel
+from massaction.model import ChemModel, nil
 import numpy as np
 
-mymodel = ChemModel(3)
-nil, vm, vo = mymodel.get_all_species()
+mymodel = ChemModel(2)
+vm, vo = mymodel.get_all_species()
 
 # Define constraints in terms of molar fractions (particles per formula unit of crystal)
-constraint_nil = nil == 1.0  # This is a dummy variable
 constraint_electroneutrality = 2 * vo - 4 * vm == 0.0
 
 # Define the reaction equation
 reaction = nil >> vm + 2 * vo
 lnK = -25.0
 
-lnc = mymodel.solve([reaction], [lnK], [constraint_nil, constraint_electroneutrality])
+lnc = mymodel.solve([reaction], [lnK], [constraint_electroneutrality])
 c = np.exp(lnc)
 print(
     "Results:"
-    f"\n\t[nil] = {c[0]:.2e}"
-    f"\n\t[vm] = {c[1]:.2e} (i.e., {c[1]*100:1.3f}% of M sites)"
-    f"\n\t[vo] = {c[2]:.2e} (i.e., {.5*c[2]*100:1.3f}% of O sites)"
+    f"\n\t[vm] = {c[0]:.2e} (i.e., {c[0]*100:1.3f}% of M sites)"
+    f"\n\t[vo] = {c[1]:.2e} (i.e., {.5*c[1]*100:1.3f}% of O sites)"
 )
